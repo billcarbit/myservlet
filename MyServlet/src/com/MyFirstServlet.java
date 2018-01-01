7ca124d1a3b2d06447457a5030dd5663cd6f6187
billcarbit <517542596@qq.com>
2018-01-01 21:09:06 +0800

    dsds

---
+++ MyServlet/src/com/MyFirstServlet.java
@@ -0,0 +1,62 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyFirstServlet", urlPatterns = {"/MyFirstServlet"})
+public class MyFirstServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -1915463532411657451L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+/*        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+// Write some content
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>MyFirstServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h2>Servlet MyFirstServlet at " + request.getContextPath() + "</h2>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }*/
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        responseJson(response);
+    }
+
+    void responseJson(HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        String jsonStr = "{\"name\":\"fly\",\"type\":\"虫子\"}";
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            out.write(jsonStr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "MyFirstServlet";
+    }
+
+}
